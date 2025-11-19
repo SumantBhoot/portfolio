4a6e4450cba528dbd1569f7b19f0ae247b4a5cc7
SumantBhoot <bhootsumant@gmail.com>
2025-11-19 13:41:34 +0530

    Add new homepage files

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('contactMe').addEventListener('click', () => {
+        window.open('https://mail.google.com/mail/?view=cm&fs=1&to=bhootsumant@gmail.com');
+    })
+})
